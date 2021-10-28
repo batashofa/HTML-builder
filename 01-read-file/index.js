fec4226cad408126dbf3a6d603aa788511b4ecfc
nastassia-batashova <batashofa@gmail.com>
2021-10-28 10:42:44 +0300

    feat: added 01-read-file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+
+const readable = fs.createReadStream(filePath, 'utf-8');
+readable.on('data', (chunk) => {process.stdout.write(chunk);});
+
