4c70d61cba56fc29e29e0f299dc95a6ae22a5d70
nastassia-batashova <batashofa@gmail.com>
2021-11-07 09:34:00 +0300

    fix: fixed bugs in 06 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -23,37 +23,70 @@ const assetsCopy = path.join(__dirname, '/project-dist/assets/');
 const Styles = path.join(__dirname, '/styles');
 
 
+fs.readdir(projectDist, (err, items) => {
+    if (!err) {
+        removeDist();
+    } else {
+        createItem();
+        createAssets();
+        creatStyleCss();
+        replaceTempTags();
+    }
+})
+
+
+async function removeDist() {
+    await fs.rm(projectDist, {force: true, recursive: true}, (err) => {
+        if (err) {
+            console.log('The folder cant remove')
+        } else {
+            createItem();
+            createAssets();
+            creatStyleCss();
+            replaceTempTags();
+        }
+    })
+}
+
 async function createItem() {
-    await fsProm.mkdir(projectDist, {recursive: true});
-    await fsProm.open(projectDistIndex, 'w');
-    await fsProm.open(projectDistStyle, 'w');
+    await fs.mkdir(projectDist, {recursive: true}, (err) => {
+        if (err) throw err;
+    });
+    await fs.open(projectDistIndex, 'w', (err) => {
+        if (err) throw err;
+    });
+    await fs.open(projectDistStyle, 'w', (err) => {
+        if (err) throw err;
+    });
+
 }
 
 async function createAssets() {
-    await fs.mkdir(assetsCopy, (err)=> {
+    await fs.mkdir(assetsCopy, (err) => {
         if (err) console.log('The folder already exists');
     });
-    await fs.readdir(assets, (err, files)=> {
+    await fs.readdir(assets, (err, files) => {
         for (const file of files) {
-            fs.mkdir(path.join(assetsCopy, file), {recursive: true}, (err)=> {
+            fs.mkdir(path.join(assetsCopy, file), {recursive: true}, (err) => {
                 if (err) throw err;
             });
             const assetsFolder = path.join(assets, file);
             const assetsCopyFolder = path.join(assetsCopy, file);
-            fs.readdir(assetsFolder, (err, items)=> {
+            fs.readdir(assetsFolder, (err, items) => {
                 for (const item of items) {
-                    fs.copyFile(path.join(assetsFolder, item), path.join(assetsCopyFolder, item), fs.constants.COPYFILE_EXCL, (err)=> {
-                        if (err) {process.exit();}
+                    fs.copyFile(path.join(assetsFolder, item), path.join(assetsCopyFolder, item), fs.constants.COPYFILE_EXCL, (err) => {
+                        if (err) {
+                            process.exit();
+                        }
                     });
                 }
             });
         }
     })
-
 }
 
 async function replaceTempTags() {
-    fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', async (e, data) => {
+    await fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', async (e, data) => {
         let template = data;
         const reg = /{{(.*)}}/;
         let tag = template.match(reg);
@@ -82,20 +115,17 @@ async function replaceTempTags() {
             }
             tag = template.match(reg)
         }
-        fs.writeFile(projectDistIndex, template, err => {
+        await fs.writeFile(projectDistIndex, template, err => {
             if (err) throw err;
         })
     })
 }
 
 async function creatStyleCss() {
-    fs.rm(projectDistStyle, {force: true}, (err)=> {
-        if (err) throw err;
-    });
-    fs.readdir(Styles, (err,files)=> {
+    await fs.readdir(Styles, (err, files) => {
         for (const file of files) {
-            if(path.extname(file) === '.css') {
-                fs.readFile(path.join(Styles,file), 'utf-8', (err, data) => {
+            if (path.extname(file) === '.css') {
+                fs.readFile(path.join(Styles, file), 'utf-8', (err, data) => {
                     if (err) {
                         process.exit();
                     }
@@ -108,7 +138,3 @@ async function creatStyleCss() {
     });
 }
 
-createItem();
-createAssets();
-creatStyleCss();
-replaceTempTags();
